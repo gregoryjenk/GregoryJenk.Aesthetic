ba03cf715f59b28ea7e143be6b7d2fa1abf03a00
Gregory Jenkins <gregoryjenk@gmail.com>
2017-09-09 00:43:55 +0100

    Starting the Gulp file implementation.

---
+++ GregoryJenk.Aesthetic/GregoryJenk.Aesthetic.Scheme.Default/gulpfile.js
@@ -0,0 +1,59 @@
+﻿/// <binding BeforeBuild="less, minify" Clean="clean" />
+
+var del = require("del");
+var gulp = require("gulp");
+var less = require("gulp-less");
+var minify = require("gulp-minify");
+
+var paths = {
+    lib: [
+        {
+            src: "./node_modules/animate.css/animate.min.css",
+            dest: "./dist/lib/animate.css/"
+        },
+        {
+            src: "./node_modules/font-awesome/css/font-awesome.css",
+            dest: "./dist/lib/font-awesome/css/"
+        },
+        {
+            src: "./node_modules/font-awesome/fonts/*",
+            dest: "./dist/lib/font-awesome/fonts/"
+        }
+    ]
+};
+
+gulp.task("clean", function () {
+    return del([
+        "./dist/css/**/*",
+        "./dist/js/**/*",
+        "./dist/lib/**/*"
+    ]);
+});
+
+gulp.task("less", function () {
+    return gulp.src("./Less/**/*.less")
+        .pipe(less())
+        .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("lib", function () {
+    for (var i = 0; i < paths.lib.length; i++) {
+        gulp.src(paths.lib[i].src).pipe(gulp.dest(paths.lib[i].dest));
+    }
+});
+
+gulp.task("minify", function () {
+    gulp.src("./dist/js/**/*.js")
+        .pipe(minify({
+            ext: {
+                src: ".js",
+                min: ".min.js"
+            },
+            exclude: ["tasks"],
+            ignoreFiles: [
+                ".combo.js",
+                "-min.js"
+            ]
+        }))
+        .pipe(gulp.dest("./wwwroot/app/js/"));
+});
